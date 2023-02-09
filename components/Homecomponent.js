1f3356bbb2444c9fbe7547609cdc22f15ae3f0e6
Antony Merle <antony.merle@gmail.com>
2023-02-09 23:07:22 +0100

    tweet input section

--- components/Homecomponent.js
+++ components/Homecomponent.js
@@ -1,15 +1,31 @@
 import React from "react";
+import { useState } from "react";
 import styles from "../styles/Homecomponent.module.css";
 import Image from "next/image";
 import logo from "../public/logo.png";
 import egg from "../public/egg.jpg";
 
 function Home() {
+  const [tweetContent, setTweetContent] = useState("");
+
   const handleLogout = () => {
     console.log("LOGOUT:", {});
-
     // TODO : reset store redux
   };
+
+  const handleTweet = () => {
+    if (!tweetTooLong()) {
+      // TODO : POST tweet to backend
+      console.log("TWEET:", {});
+    } else {
+      console.log("TWEET TO LONG");
+    }
+  };
+
+  const tweetTooLong = () => {
+    return tweetContent.length > 280;
+  };
+
   return (
     <main className={styles.container}>
       <section className={styles.leftColumn}>
@@ -27,7 +43,35 @@ function Home() {
           </button>
         </div>
       </section>
-      <section className={styles.centerColumn}>centerColumn</section>
+      <section className={styles.centerColumn}>
+        <div className={styles.newTweetContainer}>
+          <h3>Home</h3>
+          <div className={styles.inputContainer}>
+            <textarea
+              name="tweet"
+              placeholder="What's up ?"
+              id=""
+              cols="5"
+              rows="1"
+              className={styles.tweetInput}
+              onChange={(e) => setTweetContent(e.target.value)}
+            ></textarea>
+            <div className={styles.fireTweet}>
+              <p className={tweetTooLong() && styles.tweetTooLong}>
+                {tweetContent.length}/280
+              </p>
+              <button
+                className={
+                  tweetTooLong() ? styles.tweetBtnDisabled : styles.tweetBtn
+                }
+                onClick={handleTweet}
+              >
+                Tweet
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
       <section className={styles.rightColumn}>rightColumn</section>
     </main>
   );
