9f3f47a3edfa17e9613e616d5c460a8bb1175999
Antony Merle <antony.merle@gmail.com>
2023-02-09 18:40:26 +0100

    leftcolumn ok

--- components/Homecomponent.js
+++ components/Homecomponent.js
@@ -1,7 +1,36 @@
 import React from "react";
+import styles from "../styles/Homecomponent.module.css";
+import Image from "next/image";
+import logo from "../public/logo.png";
+import egg from "../public/egg.jpg";
 
 function Home() {
-  return <div>Home</div>;
+  const handleLogout = () => {
+    console.log("LOGOUT:", {});
+
+    // TODO : reset store redux
+  };
+  return (
+    <main className={styles.container}>
+      <section className={styles.leftColumn}>
+        <Image src={logo} width={50} alt="Logo" />
+        <div className={styles.userSection}>
+          <div className={styles.userContainer}>
+            <Image src={egg} width={50} alt="pp" className={styles.pp} />
+            <div className={styles.usernameContainer}>
+              <h4>John</h4>
+              <h5>@JohnCena</h5>
+            </div>
+          </div>
+          <button className={styles.logoutBtn} onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
+      </section>
+      <section className={styles.centerColumn}>centerColumn</section>
+      <section className={styles.rightColumn}>rightColumn</section>
+    </main>
+  );
 }
 
 export default Home;
