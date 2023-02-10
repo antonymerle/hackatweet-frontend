261983aecb6ae82ea7531984af3c4af2115e7760
Antony Merle <antony.merle@gmail.com>
2023-02-10 11:50:35 +0100

    layout homepage ok

--- components/Tweet.js
+++ components/Tweet.js
@@ -10,18 +10,21 @@ function Tweet({ firstname, username, tweetDate, content, likes }) {
     <div className={styles.tweet}>
       <div className={styles.tweetHeader}>
         <Image src={egg} width={50} alt="pp" className={styles.pp} />
-        <h4 className={styles.authorFirstName}>{firstname}</h4>
-        <h5 className={styles.authorUserName}>{username}</h5>
-        <p className={styles.tweetDate}>{tweetDate}</p>
+        <div className={styles.headerText}>
+          <h4 className={styles.authorFirstName}>{firstname}</h4>
+          <h5 className={styles.authorUserName}>{username}</h5>
+          <p className={styles.tweetDate}>{tweetDate}</p>
+        </div>
       </div>
       <div className={styles.tweetContent}>{content}</div>
       <div className={styles.tweetFooter}>
-        <div className="likes">
-          <faHeart />
+        <div className={styles.likes}>
+          <FontAwesomeIcon icon={faHeart} />
           <span>1</span>
         </div>
-        <faTrashCan />
-        <span>0</span>
+        <div className={styles.delete}>
+          <FontAwesomeIcon icon={faTrashCan} />
+        </div>
       </div>
     </div>
   );
